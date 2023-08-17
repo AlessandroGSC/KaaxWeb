e1d338a9a64e61e85a2c1205e945bf43587558a8
AlessandroGSC <alessandrocgs2@gmail.com>
2023-08-17 15:53:04 -0600

    [feature] configuracion para dockerizacion

--- src/api/ApiGetUsers.jsx
+++ src/api/ApiGetUsers.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const ApiGetUsers = async (config) => {
     try {
-        const response = await axios.get("http://192.168.0.26:8081/kaax/api/" + "v1/users", config)
+        const response = await axios.get("https://kaak-api.purpleforest-3ab3d7cd.westus2.azurecontainerapps.io/kaax/api/" + "v1/users", config)
         return response;
     } catch (error) {
         console.log("error en la api",error)
