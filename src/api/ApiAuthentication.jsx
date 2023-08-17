40916ed48658556ebaed4fdea420b93e2d9b583c
AlessandroGSC <alessandrocgs2@gmail.com>
2023-08-17 15:48:29 -0600

    [refactor] url api

--- src/api/ApiAuthentication.jsx
+++ src/api/ApiAuthentication.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 export const ApiAuthentication = async (data) => {
     try {
-        const response = await axios.post("http://192.168.1.140:8081/kaax/api/" + "v1/auth/authenticate", data)
+        const response = await axios.post("https://kaak-api.purpleforest-3ab3d7cd.westus2.azurecontainerapps.io/kaax/api/" + "v1/auth/authenticate", data)
         return response;
     } catch (error) {
         console.log("error en la api",error)
